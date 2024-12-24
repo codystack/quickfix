da6174192fcae07c9edab0594f67b32bd12dde6e
codystack <ebuzzadvert@gmail.com>
2024-12-24 15:23:01 +0100

    footer added

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="bg-dark pt-6 pt-md-8 position-relative" data-bs-theme="dark">
+
+	<div class="container">
+		<div class="row g-4">
+			<div class="col-lg-4 mx-auto text-center">
+				<a href="./">
+					<img class="h-70px" src="assets/images/logo-light.svg" alt="logo">
+				</a>
+				<p class="my-3 my-lg-4">We offer a comprehensive range of household services, encompassing laundry, cleaning, and car wash/fixing, in Nigeria.</p>
+				<ul class="list-inline mb-0">
+					<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-secondary" href="https://www.instagram.com/quickfix_hq" target="_blank"><i class="bi bi-facebook lh-base"></i></a> </li>
+					<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-secondary" href="https://www.instagram.com/quickfix_hq" target="_blank"><i class="bi bi-instagram lh-base"></i></a> </li>
+					<li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-secondary" href="https://www.x.com/quickfix_hq" target="_blank"><i class="bi bi-twitter-x lh-base"></i></a> </li>
+				</ul>
+			</div>
+		</div>
+
+		<hr class="mt-xl-5 mb-0 opacity-1">
+
+		<div class="d-md-flex justify-content-between align-items-center text-center text-lg-start py-4">
+			<div class="text-body small mb-3 mb-md-0"> Copyrights © <script>document.write(new Date().getFullYear());</script> QuickFix. Built by <a href="https://www.webify.com.ng" target="_blank" class="text-body text-primary-hover hover-underline-animation">Webify</a>. </div>
+
+			<ul class="nav d-flex justify-content-center gap-1 mb-0">
+				<li class="nav-item"><a class="nav-link small py-0" href="privacy">Privacy policy</a></li>
+				<li class="nav-item"><a class="nav-link small py-0 pe-0" href="terms">Terms & conditions</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+
+
+<script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/purecounterjs/dist/purecounter_vanilla.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+
+<script src="assets/js/functions.js"></script>
+
+</body>
+</html>
